610e1019b4d32fe74aa3c7f82d0747d6aec91e04
Jie Jenn <44417114+DataSolveProblems@users.noreply.github.com>
2019-05-23 15:18:52 +0800

    Create Outlook_Constants.py

---
+++ Outlook/Outlook_Constants.py
@@ -0,0 +1,25 @@
+"""
+https://docs.microsoft.com/en-us/office/vba/api/outlook.oldefaultfolders
+"""
+
+# OlDefaultFolders enumeration
+olFolderCalendar = 9
+olFolderConflicts = 19
+olFolderContacts = 10
+olFolderDeletedItems = 3
+olFolderDrafts = 16
+olFolderInbox = 6
+olFolderJournal = 11
+olFolderJunk = 23
+olFolderLocalFailures = 21
+olFolderManagedEmail = 29
+olFolderNotes = 12
+olFolderOutbox = 4
+olFolderSentMail = 5
+olFolderServerFailures = 22
+olFolderSuggestedContacts = 30
+olFolderSyncIssues = 20
+olFolderTasks = 13
+olFolderToDo = 28
+olPublicFoldersAllPublicFolders = 18
+olFolderRssFeeds = 25
