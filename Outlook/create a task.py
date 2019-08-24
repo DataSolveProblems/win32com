dc7efd065c17580e7fcc6cba7434612c8a3b303a
Jie Jenn <44417114+DataSolveProblems@users.noreply.github.com>
2019-08-24 10:53:07 +0800

    Update create a task.py

--- Outlook/create a task.py
+++ Outlook/create a task.py
@@ -7,6 +7,8 @@ olAcct = olNS.Accounts(2)
 # get currentuser
 olNS.CurrentUser()
 
+# https://docs.microsoft.com/en-us/office/vba/api/Outlook.OlDefaultFolders
+
 outlook_task_item = 3
 tskItem = olApp.CreateItem(outlook_task_item)
 tskItem.subject = "Send report"
