e85be66157b9dd688e1795c49ade7b76837c8637
Jie Jenn <44417114+DataSolveProblems@users.noreply.github.com>
2019-08-24 10:50:18 +0800

    Create create a task.py

---
+++ Outlook/create a task.py
@@ -0,0 +1,16 @@
+import win32com.client as win32
+
+# olApp = win32.Dispatch('Outlook.Application')
+olApp = win32.Dispatch('Outlook.Application')
+olNS = olApp.GetNamespace('MAPI')
+olAcct = olNS.Accounts(2)
+# get currentuser
+olNS.CurrentUser()
+
+outlook_task_item = 3
+tskItem = olApp.CreateItem(outlook_task_item)
+tskItem.subject = "Send report"
+tskItem.DueDate = '02/02/2019'
+tskItem.Categories = 'Office'
+tskItem.body = "Hello World"
+tskItem.save()
